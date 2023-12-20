90d23738da2d08149282f0a593f8d1f7f022b5aa
GiliamDatema <helmed_buddy0w@icloud.com>
2023-12-20 06:02:27 +0100

    clean up

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
+  // HTML5 mode is recommended
+  // https://router.vuejs.org/guide/essentials/history-mode.html#HTML5-Mode
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -15,7 +17,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('@/views/AboutView.vue')
     }
   ]
 })
