a49743b93229a31c648ebe846cd4c91acc1b0318
GiliamDatema <helmed_buddy0w@icloud.com>
2023-12-21 11:20:34 +0100

    add color scheme dependent theme

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <header>
     <nav>
-      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-      <RouterLink :to="{ name: 'about' }">About</RouterLink>
+      <Menubar :model="items">
+        <!-- <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+        <RouterLink :to="{ name: 'about' }">About</RouterLink> -->
+      </Menubar>
     </nav>
   </header>
 
@@ -14,15 +16,52 @@
 <script setup>
 // The `setup` attribute allows using Composition API inside SFCs.
 // The code inside is compiled as the content of the component's setup() function,
-// which will execute every time an instance of the component is created. 
+// which will execute every time an instance of the component is created.
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+import { usePrimeVue } from 'primevue/config'
+import Menubar from 'primevue/menubar'
+import router from '@/router'
+
+const PrimeVue = usePrimeVue()
+
+const getPreferredColorScheme = () => {
+  return window?.matchMedia?.('(prefers-color-scheme: dark)')?.matches ? 'dark' : 'light'
+}
+const setColorScheme = (scheme) => {
+  switch (scheme) {
+    case 'dark':
+      PrimeVue.changeTheme('lara-light-green', 'lara-dark-green', 'theme-link', () => {})
+      break;
+    case 'light':
+      PrimeVue.changeTheme('lara-dark-green', 'lara-light-green', 'theme-link', () => {})
+      break;
+    default:
+      break;
+  }
+}
+const updateColorScheme = () => setColorScheme(getPreferredColorScheme())
+const colorSchemeQuery = window?.matchMedia?.('(prefers-color-scheme: dark)')
+colorSchemeQuery.addEventListener('change', updateColorScheme)
+updateColorScheme()
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    command: () => {
+      router.push({ name: 'home' })
+    }
+  },
+  {
+    label: 'About',
+    command: () => {
+      router.push({ name: 'about' })
+    }
+  }
+])
 </script>
 
 <style lang="scss" module>
-nav a:hover,
-nav :global(a.router-link-active),
-nav :global(a.router-link-exact-active) {
-  background-color: grey;
-  cursor: pointer;
-}
+
 </style>
