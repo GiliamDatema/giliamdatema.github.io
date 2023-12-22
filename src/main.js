148c072105bc2bd0a81a4493cd671a9e91161f68
GiliamDatema <helmed_buddy0w@icloud.com>
2023-12-22 05:26:08 +0100

    add tooltip

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from '@/router'
 import PrimeVue from 'primevue/config'
+import Tooltip from 'primevue/tooltip'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
+app.use(Tooltip)
 
 app.mount('#app')
